9e918d8bbf5693322f65e2013bd998b897a0aad9
chocovayashi <c.tsujiyan727@gmail.com>
2020-09-30 18:03:07 +0900

    sample adaptive banner

--- app/src/main/java/jp/chocovayashi/adaptive_bannner_sample/MainActivity.kt
+++ app/src/main/java/jp/chocovayashi/adaptive_bannner_sample/MainActivity.kt
@@ -2,11 +2,51 @@ package jp.chocovayashi.adaptive_bannner_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adView: AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this)
+
+        adView = AdView(this)
+        ad_view_container.addView(adView)
+
+        loadBanner()
     }
+
+    private fun loadBanner() {
+        adView.adUnitId = "ca-app-pub-3940256099942544/6300978111"
+        adView.adSize = adSize
+        val adRequest = AdRequest
+            .Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build()
+        adView.loadAd(adRequest)
+    }
+
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = ad_view_container.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
 }
